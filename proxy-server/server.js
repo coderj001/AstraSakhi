509984606b7878968e17df3b79f732ed2fab1b8c
Raju Ghorai <raju.ghorai@everest.enginering>
2025-11-26 01:22:11 +0530

    feat: update server

--- proxy-server/server.js
+++ proxy-server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const userAgents = require('./user_agents');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
